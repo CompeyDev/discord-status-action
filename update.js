17e038c6267d8328f11898ed9423615d10e88a8f
Compey <hi@devcomp.tk>
2022-03-24 16:41:08 +0530

    :ambulance: Attempt to fix error

--- update.js
+++ update.js
@@ -1,27 +1,7 @@
-/** const axios = require('axios');
-
-// Make a request for a user with a given ID
-axios.get('/user?ID=12345')
-  .then(function (response) {
-    // handle success
-    console.log(response.discord_status);
-  })
-
-  **/
-
-
-/**
-const axios = require('axios');
-axios.get('https://api.lanyard.rest/v1/users/893762371770802227')
-  .then(res => {
-    console.log(res.data);
-  });  
-  **/
 const core = require('@actions/core');
 const github = require('@actions/github');
 const axios = require('axios');
 const { exec } = require('child_process');
-const file = core.getInput('file');
 
 try {
   axios.get('https://api.lanyard.rest/v1/users/893762371770802227')
@@ -29,6 +9,15 @@ try {
     const json = response.data.data
     const status = json.discord_status
     console.log(status)
+    const file = core.getInput('file');
+    exec('export file=', file, (err, stdout, stderr) => {
+      if (err) {
+        console.error(`exec error: ${err}`);
+        return;
+      }
+    
+      console.log(`${stdout}`);
+    });
     if (status == 'online') {
         exec('./replace.sh ${file} ⚪ 🟢', (err, stdout, stderr) => {
             if (err) {
