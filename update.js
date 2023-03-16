25815651b54ff33849ef66d6007a0972db72a754
Compey <hi@devcomp.xyz>
2023-03-16 16:12:34 +0530

    :recycle: full codebase refactor
    Really felt like the code quality didn't live up to my current
    standards (believe me, I've written this ages ago!), so decided to
    refactor the codebase for the better.
    
    And no, I'm still not back from my hiatus - I just really wanted to do
    this before the peak of the break :^)

--- update.js
+++ update.js
@@ -1,217 +1,46 @@
+#!/usr/bin/env node
+
+const isProd = true // This should be set to true when not a local development version.
+
 const core = require('@actions/core');
 const axios = require('axios');
-const { exec } = require('child_process');
-const id = core.getInput("id")
+const { readFileSync, writeFileSync } = require('fs');
+const id = isProd ? core.getInput("id") : "893762371770802227"
+const file = isProd ? core.getInput("file") : "README.md"
 
-try {
-  axios.get(`https://api.lanyard.rest/v1/users/${id}`)
-  .then((response) => {
-    const json = response.data.data
-    const status = json.discord_status
-    console.log("User's status:", status)
-    const file = core.getInput("file")
-    if (status == 'online') {
-        exec(`sed -i "s/⚪/🟢/g" ${file}`, (err, stdout, stderr) => {
-            if (err) {
-              console.error(`exec error: ${err}`);
-              throw new Error("Command failed.");
-              return;
-            }
-          
-            console.log(`${stdout}`);
-          });
-
-          exec(`sed -i "s/🔴/🟢/g" ${file}`, (err, stdout, stderr) => {
-            if (err) {
-              console.error(`exec error: ${err}`);
-              throw new Error("Command failed.");
-              return;
-            }
-          
-            console.log(`${stdout}`);
-          });
-          exec(`sed -i "s/🟡/🟢/g" ${file}`, (err, stdout, stderr) => {
-            if (err) {
-              console.error(`exec error: ${err}`);
-              throw new Error("Command failed.");
-              return;
-            }
-          
-            console.log(`${stdout}`);
-          });     
-          exec(`sed -i "s/🟣/🟢/g" ${file}`, (err, stdout, stderr) => {
-            if (err) {
-              console.error(`exec error: ${err}`);
-              throw new Error("Command failed.");
-              return;
-            }
-          
-            console.log(`${stdout}`);
-          });                                                        
-    }
-
-    if (status == 'dnd') {
-        exec(`sed -i "s/⚪/🔴/g" ${file}`, (err, stdout, stderr) => {
-            if (err) {
-              console.error(`exec error: ${err}`);
-              throw new Error("Command failed.");
-              return;
-            }
-          
-            console.log(`${stdout}`);
-          });
-
-          exec(`sed -i "s/🟢/🔴/g" ${file}`, (err, stdout, stderr) => {
-            if (err) {
-              console.error(`exec error: ${err}`);
-              throw new Error("Command failed.");
-              return;
-            }
-          
-            console.log(`${stdout}`);
-          });
-          exec(`sed -i "s/🟡/🔴/g" ${file}`, (err, stdout, stderr) => {
-            if (err) {
-              console.error(`exec error: ${err}`);
-              throw new Error("Command failed.");
-              return;
-            }
-          
-            console.log(`${stdout}`);
-          });           
-          exec(`sed -i "s/🟣/🔴/g" ${file}`, (err, stdout, stderr) => {
-            if (err) {
-              console.error(`exec error: ${err}`);
-              throw new Error("Command failed.");
-              return;
-            }
-          
-            console.log(`${stdout}`);
-          });                                                  
-    }  
-
-    if (status == 'idle') {
-        exec(`sed -i "s/⚪/🟡/g" ${file}`, (err, stdout, stderr) => {
-            if (err) {
-              console.error(`exec error: ${err}`);
-              throw new Error("Command failed.");
-              return;
-            }
-          
-            console.log(`${stdout}`);
-          });
-
-          exec(`sed -i "s/🟢/🟡/g" ${file}`, (err, stdout, stderr) => {
-            if (err) {
-              console.error(`exec error: ${err}`);
-              throw new Error("Command failed.");
-              return;
-            }
-          
-            console.log(`${stdout}`);
-          });
-          exec(`sed -i "s/🔴/🟡/g" ${file}`, (err, stdout, stderr) => {
-            if (err) {
-              console.error(`exec error: ${err}`);
-              throw new Error("Command failed.");
-              return;
-            }
-          
-            console.log(`${stdout}`);
-          });    
-          exec(`sed -i "s/🟣/🟡/g" ${file}`, (err, stdout, stderr) => {
-            if (err) {
-              console.error(`exec error: ${err}`);
-              throw new Error("Command failed.");
-              return;
-            }
-          
-            console.log(`${stdout}`);
-          });                                                         
-    }
-    
-    if (status == 'offline') {
-      exec(`sed -i "s/🟢/⚪/g" ${file}`, (err, stdout, stderr) => {
-          if (err) {
-            console.error(`exec error: ${err}`);
-            throw new Error("Command failed.");
-            return;
-          }
-        
-          console.log(`${stdout}`);
-        });
-
-        exec(`sed -i "s/🔴/⚪/g" ${file}`, (err, stdout, stderr) => {
-          if (err) {
-            console.error(`exec error: ${err}`);
-            throw new Error("Command failed.");
-            return;
-          }
-        
-          console.log(`${stdout}`);
-        });
-        exec(`sed -i "s/🟡/⚪/g" ${file}`, (err, stdout, stderr) => {
-          if (err) {
-            console.error(`exec error: ${err}`);
-            throw new Error("Command failed.");
-            return;
-          }
-        
-          console.log(`${stdout}`);
-        });    
-        exec(`sed -i "s/🟣/⚪/g" ${file}`, (err, stdout, stderr) => {
-          if (err) {
-            console.error(`exec error: ${err}`);
-            throw new Error("Command failed.");
-            return;
-          }
-        
-          console.log(`${stdout}`);
-        });                                                    
-  }        
-  
-  if (status == 'streaming') {
-    exec(`sed -i "s/🟢/🟣/g" ${file}`, (err, stdout, stderr) => {
-        if (err) {
-          console.error(`exec error: ${err}`);
-          throw new Error("Command failed.");
-          return;
-        }
-      
-        console.log(`${stdout}`);
-      });
-
-      exec(`sed -i "s/🔴/🟣/g" ${file}`, (err, stdout, stderr) => {
-        if (err) {
-          console.error(`exec error: ${err}`);
-          throw new Error("Command failed.");
-          return;
-        }
-      
-        console.log(`${stdout}`);
-      });
-      exec(`sed -i "s/🟡/🟣/g" ${file}`, (err, stdout, stderr) => {
-        if (err) {
-          console.error(`exec error: ${err}`);
-          throw new Error("Command failed.");
-          return;
-        }
-      
-        console.log(`${stdout}`);
-      });    
-      exec(`sed -i "s/⚪/🟣/g" ${file}`, (err, stdout, stderr) => {
-        if (err) {
-          console.error(`exec error: ${err}`);
-          throw new Error("Command failed.");
-          return;
-        }
-      
-        console.log(`${stdout}`);
-      });                                             
-}           
-  });
-}
-
-catch (error) {
-    core.setFailed(error.message);
+const deriveReplacement = (status) => {
+  const symbols = {
+    offline: "⚪",
+    online: "🟢",
+    dnd: "🔴",
+    idle: "🟡",
+    streaming: "🟣"
   }
+  const statusSymbol = symbols[status]
+
+  const matcher = /🔴|🟣|🟡|⚪|🟢/g
+
+  try {
+    var fileBuf = readFileSync(file)
+    console.log("[deriveReplacement] :: Successfully read target file.")
+  } catch (_) { throw new Error("[deriveReplacement] :: Failed to read target file.") }
+  const replacedContents = fileBuf.toString().replace(matcher, statusSymbol)
+
+  try {
+    writeFileSync(file, replacedContents)
+    console.log("[deriveReplacement] :: Successfully wrote updated contents to target file.")
+  } catch (_) { throw new Error("[deriveReplacement] :: Failed to write to target file.") }
+}
+axios.get(`https://api.lanyard.rest/v1/users/${id}`)
+  .then((response) => {
+    const fetchedRaw = response.data.data
+    const status = fetchedRaw.discord_status
+    console.log("[self] :: Successfully fetched user status -> ", status)
+
+    if (!status) throw new Error("[self] :: Invalid status type. Refusing to continue.")
+
+    deriveReplacement(status)
+  })
+  .catch((err) => {
+    isProd ? core.setFailed(err.message) : console.error(err.message)
+  })
